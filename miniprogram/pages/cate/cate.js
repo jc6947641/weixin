a31ea5aaad6536f5c8c3627aef044066bd1d5146
jc6947641 <131967693+jc6947641@users.noreply.github.com>
2023-09-14 19:21:47 +0800

    金程

--- miniprogram/pages/cate/cate.js
+++ miniprogram/pages/cate/cate.js
@@ -5,14 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    dataList:[]
+  },
+  getData(num = 5,page = 0){
+    wx.cloud.callFunction({
+      name:"getnews",
+      data:{
+        num:num,
+        page:page
+      }
+    }).then(res=>{
+      var oldData = this.data.dataList
+      var newData = oldData.concat(res.result.data);
+      this.setData({
+        dataList:newData
+      })
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getData()
   },
 
   /**
@@ -54,7 +69,8 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    var page=this.data.dataList.length
+    this.getData(5,page)
   },
 
   /**
