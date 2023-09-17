9da058cedfe0341eb812590f6ab679d3e28f916f
king12355555 <144448303+king12355555@users.noreply.github.com>
2023-09-17 19:12:58 +0800

    123

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -1,66 +1,43 @@
 // pages/my/my.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    userInfo:''
+  },
+  onLoad(){
+    let user=wx.setStorageSync('user')
+    console.log('进入小程序的index页面获取缓存',user)
+    this.setData({
+      userInfo:user
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
 
+//授权登录
+  login(){
+    wx.getUserProfile({
+      desc: '必须授权才可以继续使用',
+      success: res=> {
+        let user=res.userInfo
+        //
+        wx.setStorageSync('user', user)//缓存
+        console.log("用户信息",user)
+        this.setData({
+          userInfo:user
+        })
+      },
+      fail: res=> {
+        console.log('授权失败',res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+//退出登录
+  loginOut(){
+    this.setData({
+      userInfo:''
+    })
+    wx.setStorageSync('user', null)
   }
 })
+
+
