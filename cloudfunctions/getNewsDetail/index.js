44323a309f94ee6c9f619d59dfd7aa10214d0321
jc6947641 <131967693+jc6947641@users.noreply.github.com>
2023-09-18 20:02:38 +0800

    金程

--- cloudfunctions/getNewsDetail/index.js
+++ cloudfunctions/getNewsDetail/index.js
@@ -2,16 +2,25 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
-const db=cloud.database();  
+const db = cloud.database();
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   const { id } = event;
 
   try {
-    const db = cloud.database();
     const collection = db.collection('news');
-
     const res = await collection.doc(id).get();
+
+    // 获取新闻详情中的文本内容
+    const content = res.data.content;
+
+    // 解析文本内容并将<img>标签替换为<image>
+    const textWithImages = content.replace(/<img src='(.*?)' \/>/g, "<image src='$1' />");
+
+    // 将解析后的文本内容添加到结果中
+    res.data.content = textWithImages;
+
     return res.data;
   } catch (error) {
     console.error('获取新闻详情失败', error);
