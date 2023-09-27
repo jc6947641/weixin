945c7b4bd0c59a2b6e011ecd728034349458902a
king12355555 <144448303+king12355555@users.noreply.github.com>
2023-09-27 17:38:54 +0800

    111

--- miniprogram/pages/pinfor/pinfor.js
+++ miniprogram/pages/pinfor/pinfor.js
@@ -1,4 +1,4 @@
-const db=wx.cloud.database()
+/*const db=wx.cloud.database()
 Page({
   //存储
   data:{
@@ -44,13 +44,14 @@ Page({
     })
   },
 
-  //存储
+  //修改
   save(){
     console.log("保存")
     let openid=wx.getStorageSync("openid")
-    db.collection("userlist").add({
+    db.collection("userlist").where({
+      openid: openid
+    }).update({
       data:{
-        openid:openid,
         name:this.data.name,
         sex:this.data.sex,
         birthday:this.data.birthday,
@@ -59,12 +60,156 @@ Page({
       }
     }).then(res=>{
       wx.showToast({
-        title: '信息已保存成功',
+        title: '信息已修改成功',
       })
     })
   },
+  
+//存储
+  onLoad: function () {
+    let openid = wx.getStorageSync("openid")
+    db.collection("userlist").where({
+      openid: openid
+    }).get().then(res => {
+      let user = res.data[0]
+      this.setData({
+        name: user.name,
+        sex: user.sex,
+        birthday: user.birthday,
+        tel: user.tel,
+        address: user.address
+      })
+    })
+  }
+  
 
 
 
+  })*/
 
-  })
+  // pages/userinfo/userinfo.js
+  
+const db = wx.cloud.database()
+
+Page({
+  data: {
+    name: '',
+    sex: '',
+    birthday: '',
+    tel: '',
+    address: ''
+  },
+
+  onLoad: function () {
+    // 获取用户信息
+    this.getUserInfo()
+  },
+
+  getUserInfo() {
+    let openid = wx.getStorageSync("openid")
+    db.collection("userlist").where({
+      openid: openid
+    }).get().then(res => {
+      let user = res.data[0]
+      if (user) {
+        this.setData({
+          name: user.name,
+          sex: user.sex,
+          birthday: user.birthday,
+          tel: user.tel,
+          address: user.address
+        })
+      }
+    })
+  },
+
+  save() {
+    // 保存用户信息
+    if (this.checkInput()) {
+      let openid = wx.getStorageSync("openid")
+      let userInfo = {
+        name: this.data.name,
+        sex: this.data.sex,
+        birthday: this.data.birthday,
+        tel: this.data.tel,
+        address: this.data.address
+      }
+      db.collection("userlist").where({
+        openid: openid
+      }).update({
+        data: userInfo
+      }).then(res => {
+        wx.showToast({
+          title: '信息已保存成功',
+        })
+      })
+    }
+  },
+
+  checkInput() {
+    // 校验用户输入的信息
+    let telReg = /^[1][3,4,5,7,8][0-9]{9}$/
+    if (!this.data.name) {
+      wx.showToast({
+        title: '请输入姓名',
+        icon: 'none'
+      })
+      return false
+    } else if (!this.data.sex) {
+      wx.showToast({
+        title: '请选择性别',
+        icon: 'none'
+      })
+      return false
+    } else if (!this.data.birthday) {
+      wx.showToast({
+        title: '请选择生日',
+        icon: 'none'
+      })
+      return false
+    } else if (!this.data.tel || !telReg.test(this.data.tel)) {
+      wx.showToast({
+        title: '请输入正确的电话号码',
+        icon: 'none'
+      })
+      return false
+    } else if (!this.data.address) {
+      wx.showToast({
+        title: '请输入地址',
+        icon: 'none'
+      })
+      return false
+    }
+    return true
+  },
+
+  name(e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+
+  sex(e) {
+    this.setData({
+      sex: e.detail.value
+    })
+  },
+
+  birthday(e) {
+    this.setData({
+      birthday: e.detail.value
+    })
+  },
+
+  tel(e) {
+    this.setData({
+      tel: e.detail.value
+    })
+  },
+
+  address(e) {
+    this.setData({
+      address: e.detail.value
+    })
+  }
+})
