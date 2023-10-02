afeee1a462096655c6ba53efd24f8263fa2aa91b
jc6947641 <131967693+jc6947641@users.noreply.github.com>
2023-10-02 18:12:33 +0800

    122

--- miniprogram/pages/pillDetail/pillDetail.js
+++ miniprogram/pages/pillDetail/pillDetail.js
@@ -25,53 +25,27 @@ Page({
     });
   },
 
-  addToCart: function () {
-    const { id, name, price } = this.data.pillDetail; // 从商品详情页获取商品信息
-    const userId = wx.getStorageSync('userId'); // 获取当前登录用户的ID，假设使用缓存存储用户ID
-
-    // 构建购物车商品对象
-    const cartItem = { id, name, price, quantity: 1, userId };
-    
-    const cart = wx.getStorageSync('cart') || [];
-
-    // 检查购物车中是否已存在相同商品，如果存在，则更新数量
-    const existingItemIndex = cart.findIndex(item => item.id === id);
-    if (existingItemIndex !== -1) {
-      cart[existingItemIndex].quantity++;
-    } else {
-      // 否则，将商品添加到购物车
-      cart.push(cartItem);
-    }
-
-    // 更新购物车数据并提示用户
-    wx.setStorageSync('cart', cart);
-    wx.showToast({
-      title: '已添加到购物车',
-      icon: 'success',
-    });
-
-    // 跳转到购物车页面
-
-    // 调用云函数或其他逻辑将商品添加到购物车
-    wx.cloud.callFunction({
-      name: 'addToCart',
-      data: cartItem,
-      success: res => {
-        wx.showToast({
-          title: '已添加到购物车',
-          icon: 'success',
-        });
-      },
-      fail: error => {
-        console.error('添加到购物车失败：', error);
-        // 在失败情况下可以添加适当的用户提示或错误处理逻辑
-      },
-    });
-  },
-
   gotoWechatPay: function () {
     wx.navigateTo({
       url: '/pages/wechatpay/wechatpay',
     });
   },
+
+  addToCart() {
+    const cartItem = {
+      id: this.data.pillDetail.id, // 商品ID
+      name: this.data.pillDetail.name, // 商品名称
+      price: this.data.pillDetail.price, // 商品价格
+      quantity: 1 // 初始数量为1
+    };
+  
+    // 将商品添加到购物车全局数据
+    getApp().globalData.cart.push(cartItem);
+  
+    // 更新成功后显示提示
+    wx.showToast({
+      title: '已加入购物车',
+      icon: 'success'
+    });
+  }
 });
