12f129b9f4ce69bcbd14d7666f0c6d866b7bced8
king12355555 <144448303+king12355555@users.noreply.github.com>
2023-10-05 13:11:06 +0800

    123

--- miniprogram/pages/pinfor/pinfor.js
+++ miniprogram/pages/pinfor/pinfor.js
@@ -1,317 +1,4 @@
-/*const db=wx.cloud.database()
-Page({
-  //存储
-  data:{
-    name:'',
-    sex:'',
-    birthday:'',
-    tel:'',
-    address:''
-  },
-
-  name(e){
-    console.log("获取姓名",e.detail.value)
-    this.setData({
-      name:e.detail.value
-    })
-  },
-
-  sex(e){
-    console.log("获取性别",e.detail.value)
-    this.setData({
-      sex:e.detail.value
-    })
-  },
-
-  birthday(e){
-    console.log("获取生日",e.detail.value)
-    this.setData({
-      birthday:e.detail.value
-    })
-  },
-
-  tel(e){
-    console.log("获取电话",e.detail.value)
-    this.setData({
-      tel:e.detail.value
-    })
-  },
-
-  address(e){
-    console.log("获取地址",e.detail.value)
-    this.setData({
-      address:e.detail.value
-    })
-  },
-
-  //修改
-  save(){
-    console.log("保存")
-    let openid=wx.getStorageSync("openid")
-    db.collection("userlist").where({
-      openid: openid
-    }).update({
-      data:{
-        name:this.data.name,
-        sex:this.data.sex,
-        birthday:this.data.birthday,
-        tel:this.data.tel,
-        address:this.data.address,
-      }
-    }).then(res=>{
-      wx.showToast({
-        title: '信息已修改成功',
-      })
-    })
-  },
-  
-//存储
-  onLoad: function () {
-    let openid = wx.getStorageSync("openid")
-    db.collection("userlist").where({
-      openid: openid
-    }).get().then(res => {
-      let user = res.data[0]
-      this.setData({
-        name: user.name,
-        sex: user.sex,
-        birthday: user.birthday,
-        tel: user.tel,
-        address: user.address
-      })
-    })
-  }
-  
-
-
-
-  })*/
-
-  // pages/userinfo/userinfo.js
-  
-//const db = wx.cloud.database()
-
-
-
-
-//失败
-/*Page({
-
-  data: {
-    name: '',
-    sex: '',
-    birthday: '',
-    tel: '',
-    address: ''
-  },
-
-  onLoad: function () {
-    // 获取用户信息
-    this.getUserInfo()
-  },
-
-  getUserInfo() {
-    let openid = wx.getStorageSync("openid")
-    db.collection("userlist").where({
-      openid: openid
-    }).get().then(res => {
-      let user = res.data[0]
-      if (user) {
-        this.setData({
-          name: user.name,
-          sex: user.sex,
-          birthday: user.birthday,
-          tel: user.tel,
-          address: user.address
-        })
-      }
-    })
-  },
-
-  save() {
-    // 保存用户信息
-    if (this.checkInput()) {
-      let openid = wx.getStorageSync("openid")
-      let userInfo = {
-        name: this.data.name,
-        sex: this.data.sex,
-        birthday: this.data.birthday,
-        tel: this.data.tel,
-        address: this.data.address
-      }
-      db.collection("userlist").where({
-        openid: openid
-      }).update({
-        data: userInfo
-      }).then(res => {
-        wx.showToast({
-          title: '信息已保存成功',
-        })
-      })
-    }
-  },
-
-  checkInput() {
-    // 校验用户输入的信息
-    let telReg = /^[1][3,4,5,7,8][0-9]{9}$/
-    if (!this.data.name) {
-      wx.showToast({
-        title: '请输入姓名',
-        icon: 'none'
-      })
-      return false
-    } else if (!this.data.sex) {
-      wx.showToast({
-        title: '请选择性别',
-        icon: 'none'
-      })
-      return false
-    } else if (!this.data.birthday) {
-      wx.showToast({
-        title: '请选择生日',
-        icon: 'none'
-      })
-      return false
-    } else if (!this.data.tel || !telReg.test(this.data.tel)) {
-      wx.showToast({
-        title: '请输入正确的电话号码',
-        icon: 'none'
-      })
-      return false
-    } else if (!this.data.address) {
-      wx.showToast({
-        title: '请输入地址',
-        icon: 'none'
-      })
-      return false
-    }
-    return true
-  },
-
-  name(e) {
-    this.setData({
-      name: e.detail.value
-    })
-  },
-
-  sex(e) {
-    this.setData({
-      sex: e.detail.value
-    })
-  },
-
-  birthday(e) {
-    this.setData({
-      birthday: e.detail.value
-    })
-  },
-
-  tel(e) {
-    this.setData({
-      tel: e.detail.value
-    })
-  },
-
-  address(e) {
-    this.setData({
-      address: e.detail.value
-    })
-  }
-
-  // 用户输入标题
-
-   
-
-  inputName: function (e) {
-    this.setData({
-      name: e.detail.value
-    });
-  },
-
-  // 用户输入作者
-  inputSex: function (e) {
-    this.setData({
-      sex: e.detail.value
-    });
-  },
-
-  // 用户输入日期
-  inputBirthday: function (e) {
-    this.setData({
-      birthday: e.detail.value
-    });
-  },
-
-  // 用户输入点击量
-  inputTel: function (e) {
-    this.setData({
-      tel: e.detail.value
-    });
-  },
-
-  // 用户输入内容
-  inputAddress: function (e) {
-    this.setData({
-      address: e.detail.value
-    });
-  },
-  
-
-
-  uploadToDatabase: function () {
-    // 初始化云开发
-    wx.cloud.init({
-      env: 'cloud1-8gwwknkg2de4d56d', // 小程序环境 ID
-    });
-
-    // 上传数据到云数据库
-    wx.cloud.database().collection('information').add({
-      data: {
-        name: this.data.name,
-        sex: this.data.sex,
-        birthday: this.data.birthday,
-        tel: this.data.tel,
-        address: this.data.address,
-        
-      },
-      success: function (res) {
-        wx.showToast({
-          title: '上传成功',
-          icon: 'success',
-          duration: 2000
-        });
-      },
-      fail: function (err) {
-        console.error('上传失败', err);
-        wx.showToast({
-          title: '上传失败',
-          icon: 'none',
-          duration: 2000
-        });
-      }
-    });
-  }})*/
-
-  //计划2
-
-//index.jsDB.get({
-    
-//获取应用实例
-/*page({
-  getData(){
-  DB.get({
-    success(res){
-    console.log("查询成功",res)
-  },
-    fail(res){
-      console.log("查询失败",res)
-    }
-  })
-}
-})*/
-
-
-const app = getApp()
+/*const app = getApp()
 //数据库初始化
 const DB = wx.cloud.database().collection("list")
 let name = ""
@@ -319,68 +6,359 @@ let age = ""
 let sex = ""
 let tel = ""
 let address = ""
+let delId = "" // 将delId变量的初始值设为空
 
 Page({
-
-//获取用户输入的名字
-addName(event){
-  name = event.detail.value
-},
-//获取用户输入的年龄
-addAge(event){
-  age = event.detail.value
-},
-//性别
-addSex(event) {
-  sex= event.detail.value
-},
-//电话
-addTel(event) {
-    tel= event.detail.value
-},
-//地址
-addAddress(event) {
-    address=event.detail.value
-},
-
-//添加数据
-addData(){
-  DB.add({
-    data:{
-      name: name,
-      age: age,
-      sex:sex,
-      tel:tel,
-      address:address
-    },
-    success(res){
-      console.log("添加成功", res)
-    },
-    fail(res){
-      console.log("添加失败", res)
-    }
-  })
-},
-
-//查询数据
-getData(){
-  DB.get({
-    success(res){
-      console.log("查询成功", res)
-    },
-    fail(res){
-      console.log("查询失败", res)
-    }
-  })
-}
-
-})
   
+  //获取用户输入的名字
+  addName(event){
+    name = event.detail.value
+  },
+  //获取用户输入的年龄
+  addAge(event){
+    age = event.detail.value
+  },
+  //性别
+  addSex(event) {
+    sex= event.detail.value
+  },
+  //电话
+  addTel(event) {
+      tel= event.detail.value
+  },
+  //地址
+  addAddress(event) {
+      address=event.detail.value
+  },
 
+  //添加数据
+  addData(){
+    DB.add({
+      data:{
+        name: name,
+        age: age,
+        sex:sex,
+        tel:tel,
+        address:address
+      },
+      success(res){
+        console.log("添加成功", res)
+      },
+      fail(res){
+        console.log("添加失败", res)
+      }
+    })
+  },
 
+  //查询数据
+  getData(){
+    DB.get({
+      success(res){
+        console.log("查询成功", res)
+      },
+      fail(res){
+        console.log("查询失败", res)
+      }
+    })
+  },
 
+  //获取用户要更新的id
+  updateId(event){
+    delId = event.detail.value
+  },
 
+  //获取用户要更新的名字
+  updateName(event){
+    name = event.detail.value
+  },
+  //获取用户要更新的年龄
+  updateAge(event){
+    age = event.detail.value
+  },
 
+  //性别
+  updateSex(event) {
+    sex= event.detail.value
+  },
+  //电话
+  updateTel(event) {
+    tel= event.detail.value
+  },
+  //地址
+  updateAddress(event) {
+    address=event.detail.value
+  },
 
+  //修改数据
+  upData(){
+    DB.doc(delId).update({
+      data:{
+        name: name,
+        age: age,
+        sex:sex,
+        tel:tel,
+        address:address
+      },
+      success(res){
+        console.log("修改成功", res)
+      },
+      fail(res){
+        console.log("修改失败", res)
+      }
+    })
+  }
 
+})*/
 
+/*Page({
+  data: {
+  name: '',
+  sex: '',
+  age: '',
+  tel: '',
+  address: '',
+  dataList: [],
+  curIndex: -1,
+  },
+  // 添加姓名
+  addName(e) {
+  this.setData({
+  name: e.detail.value
+  })
+  },
+  // 添加性别
+  addSex(e) {
+  this.setData({
+  sex: e.detail.value
+  })
+  },
+  // 添加年龄
+  addAge(e) {
+  this.setData({
+  age: e.detail.value
+  })
+  },
+  // 添加电话
+  addTel(e) {
+  this.setData({
+  tel: e.detail.value
+  })
+  },
+  // 添加地址
+  addAddress(e) {
+  this.setData({
+  address: e.detail.value
+  })
+  },
+  // 点击“确定并保存”按钮，保存数据
+  addData() {
+  let dataList = wx.getStorageSync('dataList') || []
+  let data = {
+  name: this.data.name,
+  sex: this.data.sex,
+  age: this.data.age,
+  tel: this.data.tel,
+  address: this.data.address
+  }
+  dataList.push(data)
+  wx.setStorageSync('dataList', dataList)
+  this.setData({
+  name: '',
+  sex: '',
+  age: '',
+  tel: '',
+  address: ''
+  })
+  },
+  // 点击“查询”按钮，获取已保存的数据
+  getData() {
+  let dataList = wx.getStorageSync('dataList') || []
+  this.setData({
+  dataList: dataList
+  })
+  },
+  // 点击“修改”按钮，根据姓名获取对应的数据
+  upData() {
+  let dataList = wx.getStorageSync('dataList') || []
+  let index = -1
+  for (let i = 0; i < dataList.length; i++) {
+  if (dataList[i].name == this.data.name) {
+  index = i
+  break
+  }
+  }
+  if (index >= 0) {
+  this.setData({
+  curIndex: index,
+  sex: dataList[index].sex,
+  age: dataList[index].age,
+  tel: dataList[index].tel,
+  address: dataList[index].address
+  })
+  } else {
+  wx.showToast({
+  title: '未找到对应数据',
+  icon: 'none'
+  })
+  }
+  },
+  // 点击“更新”按钮，更新数据
+  updateData() {
+    let dataList = wx.getStorageSync('dataList') || []
+    if (this.data.curIndex >= 0) {
+    dataList[this.data.curIndex].sex = this.data.sex
+    dataList[this.data.curIndex].age = this.data.age
+    dataList[this.data.curIndex].tel = this.data.tel
+    dataList[this.data.curIndex].address = this.data.address
+    wx.setStorageSync('dataList', dataList)
+    this.setData({
+    curIndex: -1,
+    sex: '',
+    age: '',
+    tel: '',
+    address: '',
+    dataList: dataList
+    })
+    } else {
+    wx.showToast({
+    title: '未找到对应数据',
+    icon: 'none'
+    })
+    }
+    }
+  })*/
+
+ 
+  
+  Page({
+    data: {
+    dataList: [], // 数据列表
+    name: '', // 姓名
+    sex: '', // 性别
+    age: '', // 年龄
+    tel: '', // 电话
+    address: '', // 地址
+    id: '' // 当前数据id
+    },
+    // 存储数据
+    addData: function () {
+    const db = wx.cloud.database() // 获取数据库实例
+    const dataList = db.collection('dataList') // 获取集合实例
+    dataList.add({
+    data: {
+    name: this.data.name,
+    sex: this.data.sex,
+    age: this.data.age,
+    tel: this.data.tel,
+    address: this.data.address
+    },
+    success: res => {
+    wx.showToast({
+    title: '保存成功',
+    })
+    this.getDataList() // 保存成功后刷新列表展示
+    },
+    fail: err => {
+    wx.showToast({
+    title: '保存失败',
+    icon: 'none'
+    })
+    console.error('[数据库] [新增记录] 失败：', err)
+    }
+    })
+    },
+    // 修改数据
+    upData: function () {
+    const db = wx.cloud.database() // 获取数据库实例
+    const dataList = db.collection('dataList') // 获取集合实例
+    dataList.doc(this.data.id).update({
+    data: {
+    name: this.data.name,
+    sex: this.data.sex,
+    age: this.data.age,
+    tel: this.data.tel,
+    address: this.data.address
+    },
+    success: res => {
+    wx.showToast({
+    title: '修改成功',
+    })
+    this.getDataList() // 修改成功后刷新列表展示
+    },
+    fail: err => {
+    wx.showToast({
+    title: '修改失败',
+    icon: 'none'
+    })
+    console.error('[数据库] [修改记录] 失败：', err)
+    }
+    })
+    },
+    // 更新数据列表
+    getDataList: function () {
+    const db = wx.cloud.database() // 获取数据库实例
+    const dataList = db.collection('dataList') // 获取集合实例
+    dataList.get({
+    success: res => {
+    this.setData({
+    dataList: res.data
+    })
+    },
+    fail: err => {
+    console.error('[数据库] [查询记录] 失败：', err)
+    }
+    })
+    },
+    // 监听姓名输入框
+    addName: function (e) {
+    this.setData({
+    name: e.detail.value
+    })
+    },
+    // 监听性别输入框
+    addSex: function (e) {
+    this.setData({
+    sex: e.detail.value
+    })
+    },
+    // 监听年龄输入框
+    addAge: function (e) {
+    this.setData({
+    age: e.detail.value
+    })
+    },
+    // 监听电话输入框
+    addTel: function (e) {
+    this.setData({
+    tel: e.detail.value
+    })
+    },
+    // 监听地址输入框
+    addAddress: function (e) {
+    this.setData({
+    address: e.detail.value
+    })
+    },
+    // 监听列表项点击事件
+    onItemClick: function (e) {
+    const id = e.currentTarget.dataset.id
+    const name = e.currentTarget.dataset.name
+    const sex = e.currentTarget.dataset.sex
+    const age = e.currentTarget.dataset.age
+    const tel = e.currentTarget.dataset.tel
+    const address = e.currentTarget.dataset.address
+    this.setData({
+    id: id,
+    name: name,
+    sex: sex,
+    age: age,
+    tel: tel,
+    address: address
+    })
+    },
+    onLoad: function () {
+    this.getDataList() // 加载页面时获取数据列表
+    }
+    })
+  
+  
