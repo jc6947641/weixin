57751612bd0349a7b608c2fd7df5a855cc213fe0
Lilinii <131882645+Lilinii@users.noreply.github.com>
2023-10-08 19:16:25 +0800

    li

--- miniprogram/pages/wechatpay/wechatpay.js
+++ miniprogram/pages/wechatpay/wechatpay.js
@@ -1,37 +1,17 @@
 Page({
-  onLoad(options) {
-    const price = options.price;
-    const title = options.title;
-    const up1 = options.up1;
-    this.setData({
-      goods: {
-        name: title,
-        cover: up1,
-        price: price
-      }
-    })
+  data:{
+    title:'',
+    price:'',
+    up1:''
   },
-  onPay() {
-    const price = this.data.goods.price;
-    // 调用微信支付接口
-    wx.requestPayment({
-      nonceStr: '随机字符串',
-      package: '统一下单接口返回的 prepay_id',
-      paySign: '签名',
-      timeStamp: '时间戳',
-      success(res) {
-        // 支付成功，跳转到订单页面
-        wx.navigateTo({
-          url: '/pages/order/index'
-        })
-      },
-      fail(res) {
-        // 支付失败，提示用户
-        wx.showToast({
-          title: '支付失败',
-          icon: 'none'
-        })
-      }
+
+  onLoad:function(options) {
+    console.log(options)
+
+    this.setData({
+      title:options.title,
+      up1:options.up1,
+      price:options.price
     })
   }
 })
