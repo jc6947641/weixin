fc1367514ef0ff4f2e468f59832f05e9a5a4beeb
Lilinii <131882645+Lilinii@users.noreply.github.com>
2023-10-09 20:00:51 +0800

    li

--- miniprogram/pages/wechatpay/wechatpay.js
+++ miniprogram/pages/wechatpay/wechatpay.js
@@ -14,7 +14,13 @@ Page({
       price:options.price
     })
   },
-  
+
+  onPay() {
+    // 支付逻辑，成功后跳转到 paySuccess 页面
+    wx.navigateTo({
+      url: '/pages/paySuccess/paySuccess'
+    })
+  }
 })
 
 
